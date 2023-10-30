aeb9bce0b0c350b74ac876661be04d7892eb9207
JAG008 <122801142+JAG008@users.noreply.github.com>
2023-10-30 12:22:08 -0400

    Update client_queries.py
    finished

--- client_queries.py
+++ client_queries.py
@@ -52,19 +52,6 @@ ALTER TABLE IF EXISTS public.schmackages
     OWNER to postgres;'''
 
 
-#Finds the best customer in the past year
-def most_frequent_customer():
-    cursor.execute("""SELECT shipper_id FROM schmackage_logs, schmackages
-                   WHERE schmackage_logs.tracking_number = schmackages.tracking_number
-                   GROUP BY shipper_id ORDER BY price LIMIT 1;"""
-    return cursor.fetchone()
-
-#Finds the customer that spent the most money in the past year
-def most_spent_customer():
-    cursor.execute("""SELECT shipper_id FROM schmackages
-                   GROUP BY shipper_id ORDER BY SUM(price) LIMIT 1;"""
-    return cursor.fetchone()
-
 #Generates the report in response to truck_number crashing
 def truck_crash_report(truck_number):
     truck_report_data = {'shipper_data': [], 'recipient_data': [], 'last_delivery': []}
@@ -84,6 +71,46 @@ def truck_crash_report(truck_number):
                    WHERE truck = {truck_number} GROUP BY max(timestamp) LIMIT 1""")
     truck_report_data['last_delivery'] = cursor.fetchall()
     return truck_report_data
+
+#Finds the best customer in the past year
+def most_frequent_customer():
+    cursor.execute("""SELECT shipper_id FROM schmackage_logs, schmackages
+                   WHERE schmackage_logs.tracking_number = schmackages.tracking_number AND
+                   DATEPART(yyyy, timestamp) = DATEPART(yyyy, DATEADD(m, -12, getdata()))
+                   GROUP BY shipper_id ORDER BY price LIMIT 1;""")
+    return cursor.fetchone()
+
+#Finds the customer that spent the most money in the past year
+def most_spent_customer():
+    cursor.execute("""SELECT shipper_id FROM schmackages
+                   WHERE DATEPART(yyyy, timestamp) = DATEPART(yyyy, DATEADD(m, -12, getdata()))
+                   GROUP BY shipper_id ORDER BY SUM(price) LIMIT 1;""")
+    return cursor.fetchone()
+
+#Bills the customers for the past moneth of services
+#'simple_bill' => customer, address, and amount owed
+#'itemized_bill' => bill listing each individual shipment and the chargest for it
+def bill_customers(bill_type = 'simple_bill'):
+
+    #Creates simple bill
+    if bill_type == 'simple_bill':
+        cursor.execute("""SELECT shipper_id, return_address, SUM(price) FROM schmackages
+                       WHERE DATEPART(m, timestamp) = DATEPART(m, DATEADD(m, -1, getdate())) AND
+                       DATEPART(yyyy, timestamp) = DATEPART(yyyy, DATEADD(m, -1, getdate()))
+                       GROUP BY shipper_id""")
+
+    #Gives bill type by item
+    elif bill_type == 'itemized_bill':
+        cursor.execute("""SELECT shipper_id, tracking_number, price FROM schmackages, schmackage_logs
+                       WHERE schmackages.tracking_number = schmackage_logs.tracking_number AND
+                       DATEPART(m, timestamp) = DATEPART(m, DATEADD(m, -1, getdate())) AND
+                       DATEPART(yyyy, timestamp) = DATEPART(yyyy, DATEADD(m, -1, getdate()))
+                       GROUP BY shipper_id""")
+
+    #Returns error message when invalid bill type is used
+    else:
+        return '''Error, invalid bill_type, please choose "simple_bill" or "itemized_bill"'''
+    return cursor.fetchall()
 
 #Forms connection and sets up cursor to access SHAMAZON
 conn = p.connect(database = 'shamazon',
